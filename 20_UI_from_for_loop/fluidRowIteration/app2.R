31b15879c9a31acf231307bdbe23d9f45cdbe12c
IgnasiPuigUPC <ignasi.puig@upc.edu>
2023-01-24 19:16:15 +0100

    Update 20_UI_from_for_loop
    1. Included directory fluidRowIteration
    2. Included shiny file app2.R for dinamycally create selectInput based
       on countries chosen.
    3. Included shiny file app3.R to dinamycally create a set of input boxes
       based on a choice of number of boxes

---
+++ 20_UI_from_for_loop/fluidRowIteration/app2.R
@@ -0,0 +1,70 @@
+#
+# selectInput boxes created dinamycally based on the countries selected
+#
+
+library(shiny)
+library(shinydashboard)
+
+ui <- fluidPage(
+    
+    h1('Foor loop test'),
+    br(),
+    
+    title = 'Foor loop UI',
+        selectInput(
+            inputId = "select",
+            label = "Select countries:",
+            choices = c("CH", "JP", "GER"),
+            multiple = TRUE),
+    
+    column(4, uiOutput(outputId = "ui1")),
+    column(4, uiOutput(outputId = "ui2")),
+    column(4, uiOutput(outputId = "ui3"))
+    
+    # dashboardBody(column(4, uiOutput("ui1")),
+    #               column(4, uiOutput("ui2")),
+    #               column(4, uiOutput("ui3")))
+)
+
+server <- function(input, output) {
+    
+    output$ui1 <- renderUI({
+        req(input$select)
+
+        lapply(seq_along(input$select), function(i) {
+            # fluidRow(
+                renderUI(p(input$select[i]))
+            # )
+        })
+    })
+
+    output$ui2 <- renderUI({
+        req(input$select)
+
+        lapply(seq_along(input$select), function(i) {
+            fluidRow(
+                renderUI(
+                    textInput(inputId = 'entry',
+                              label = input$select[i],
+                              value = input$select[i])
+                )
+            )
+        })
+    })
+
+    output$ui3 <- renderUI({
+        req(input$select)
+        
+        lapply(seq_along(input$select), function(i) {
+            fluidRow(
+                renderUI(
+                    textInput(inputId = 'entry',
+                              label = input$select[i],
+                              value = input$select[i])
+                )
+            )
+        })
+    })
+}
+
+shinyApp(ui = ui, server = server)
---
+++ 20_UI_from_for_loop/fluidRowIteration/app3.R
@@ -0,0 +1,86 @@
+#
+# The number of textInput and their labels is dinamycally created via lapply 
+#
+
+library(shiny)
+library(shinydashboard)
+
+ui <- fluidPage(
+    
+    h1('Foor loop test'),
+    br(),
+    
+ 
+    column (4, title = 'Foor loop UI',
+            numericInput(inputId = "select",
+                         label = "Select no of rows:",
+                         value = 5)
+        ),
+    
+    fluidRow(),
+    
+    column(4, uiOutput(outputId = 'ui1')),
+    column(4, uiOutput(outputId = 'ui2')),
+    column(4, uiOutput(outputId = 'ui3'))
+    
+    # column(4, 
+    #        lapply(1:5, 
+    #               function(i) {
+    #                   textInput(inputId = paste0('conc',i),
+    #                             label = paste('concentration',i, sep =' '))}
+    #        )
+    # ),
+    # 
+    # column(4, 
+    #        lapply(1:5, 
+    #               function(i) {
+    #                   textInput(inputId = paste0('units',i),
+    #                             label = paste('units',i, sep =' '))}
+    #        )
+    # )
+    
+    # column(4, uiOutput(outputId = "ui2")),
+    # column(4, uiOutput(outputId = "ui3"))
+    
+    # dashboardBody(column(4, uiOutput("ui1")),
+    #               column(4, uiOutput("ui2")),
+    #               column(4, uiOutput("ui3")))
+)
+
+server <- function(input, output) {
+    
+    output$ui1 <- renderUI({
+        req(input$select)
+
+        lapply(1:input$select, function(i) {
+               renderUI(
+                    textInput(inputId = paste0('test',i),
+                              label = paste('test',i, sep =' '))
+                )
+        })
+    })
+
+    output$ui2 <- renderUI({
+        req(input$select)
+        
+        lapply(1:input$select, function(i) {
+            renderUI(
+                textInput(inputId = paste0('conc',i),
+                          label = paste('concentration',i, sep =' '))
+            )
+        })
+    })
+    
+    output$ui3 <- renderUI({
+        req(input$select)
+        
+        lapply(1:input$select, function(i) {
+            renderUI(
+                textInput(inputId = paste0('unit',i),
+                          label = paste('units',i, sep =' '))
+            )
+        })
+    })
+}
+
+shinyApp(ui = ui, server = server)
